2054d7d3ecff2fb9bc9407210b61781ce458eab1
SirNando <matiasnandin@hotmail.com>
2023-06-09 16:11:07 -0300

    Updated structure and added minimal styling

---
+++ app.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const express = require('express');
+
+const accessRoutes = require('./routes/access');
+
+const app = express();
+
+// Activate EJS view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true })); // Parse incoming request bodies
+app.use(express.static('public')); // Serve static files (e.g. CSS files)
+
+app.use(accessRoutes);
+
+
+app.use(function (error, req, res, next) {
+  // Default error handling function
+  // Will become active whenever any route / middleware crashes
+  console.log(error);
+  res.status(500).render('500');
+});
+
+app.listen(3000);
